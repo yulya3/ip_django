dbcff3d04fec75a38e4b9c9a32115d35f84eddf4
yulya3 <b.yss@mail.ru>
2024-04-01 14:17:20 +0500

    delete form for mysite

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -40,3 +40,13 @@ def update_item(request, my_id):
         return redirect("/myapp/")
     context = {"item": item}
     return render(request, "myapp/updateitem.html", context)
+
+def delete_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == "POST":
+        item.delete()
+        return redirect("/myapp/")
+    context = {"item": item}
+    return render(request, "myapp/deleteitem.html", context)
+
+
