a9ed462f73dd0d5c864bd12af5b0284b7da05e9d
yulya3 <b.yss@mail.ru>
2024-04-01 09:23:58 +0500

    navbar.html creater separetely

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static 
+from django.conf import settings
 
 
 urlpatterns = [
@@ -8,3 +9,5 @@ urlpatterns = [
     path("myapp/", include("myapp.urls", namespace="myapp") )
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
